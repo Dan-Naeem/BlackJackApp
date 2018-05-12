10a246dbee44c44e96b9e4bb7a54141767c3319c
Dan Naeem <dan.naeem95@gmail.com>
2018-05-11 23:06:15 -0400

    added onto onCreate() and onUpgrade() in database helper class

--- app/src/main/java/com/example/dan/blackjackltd/DatabaseHelper.java
+++ app/src/main/java/com/example/dan/blackjackltd/DatabaseHelper.java
@@ -12,23 +12,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "statistics.db";
     public static final String TABLE_NAME = "stats_table";
-    public static final String COL_1 = "game_type";
-    public static final String COL_2 = "game_results";
-    public static final String COL_3 = "player1_hand";
-    public static final String COL_4 = "player2_hand";
-    public static final String COL_5 = "dealer_hand";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "TYPE";
+    public static final String COL_3 = "RESULTS";
+    public static final String COL_4 = "P1";
+    public static final String COL_5 = "P2";
+    public static final String COL_6 = "DEALER";
 
     public DatabaseHelper(Context context, ) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER  PRIMARY KEY AUTOINCREMENT, " +
+                "TYPE TEXT, RESULTS TEXT, P1 INTEGER, P2 INTEGER, DEALER INTEGER) ");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
