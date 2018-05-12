6402ae4c542eedd13b97dd068e69a6760c45163c
Dan Naeem <dan.naeem95@gmail.com>
2018-05-12 00:42:30 -0400

    created viewAll() in StatsAct that pulls data from the db

--- app/src/main/java/com/example/dan/blackjackltd/StatsAct.java
+++ app/src/main/java/com/example/dan/blackjackltd/StatsAct.java
@@ -1,6 +1,8 @@
 package com.example.dan.blackjackltd;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 
 /**
@@ -9,9 +11,48 @@ import android.os.Bundle;
 
 public class StatsAct extends Activity {
 
+    DatabaseHelper myDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stats_layout);
+        myDb = new DatabaseHelper(this);
+
+        viewAll();
+    }
+
+    public void viewAll() {
+       Cursor res = myDb.getAllData();
+
+       //if no data available
+       if(res.getCount() == 0){
+           //show message
+           showMessage("Error", "Nothing Found");
+           return;
+       }
+       else{
+           StringBuffer buffer = new StringBuffer();
+
+           while( res.moveToNext() ) {
+               buffer.append("ID : " + res.getString(0) + "\n");
+               buffer.append("TYPE : " + res.getString(1) + "\n");
+               buffer.append("RESULTS : " + res.getString(2) + "\n");
+               buffer.append("P1 : " + res.getString(3) + "\n");
+               buffer.append("P2 : " + res.getString(4) + "\n");
+               buffer.append("DEALER : " + res.getString(5) + "\n\n");
+           }
+
+           //show all data
+           showMessage( "Data", buffer.toString() );
+       }
+    }
+
+    public void showMessage (String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
