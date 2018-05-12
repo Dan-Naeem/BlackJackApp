b4f8eba3cefe6a6b87d83a6eb3192be18b55ac86
Dan Naeem <dan.naeem95@gmail.com>
2018-05-12 00:21:47 -0400

    created getAllData() for DatabaseHelper

--- app/src/main/java/com/example/dan/blackjackltd/DatabaseHelper.java
+++ app/src/main/java/com/example/dan/blackjackltd/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.dan.blackjackltd;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -51,5 +52,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
+    }
+
+    public Cursor getAllData() {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
