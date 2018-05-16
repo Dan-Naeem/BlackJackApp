c7cb274f429c30c7b3e7d207f56a36fee061fc40
Dan Naeem <dan.naeem95@gmail.com>
2018-05-16 19:58:40 -0400

    created a localMatch activity, styled it, allowed to swithc with intent from coopMission

--- app/src/main/java/com/example/dan/blackjackltd/CoopMission.java
+++ app/src/main/java/com/example/dan/blackjackltd/CoopMission.java
@@ -1,7 +1,9 @@
 package com.example.dan.blackjackltd;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by DAN on 3/29/18.
@@ -13,5 +15,9 @@ public class CoopMission extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coop_mission);
+    }
+
+    public void localButtonClicked(View view){
+      startActivity(new Intent(this, LocalMatch.class ));
     }
 }
---
+++ app/src/main/java/com/example/dan/blackjackltd/LocalMatch.java
@@ -0,0 +1,18 @@
+package com.example.dan.blackjackltd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by DAN on 5/16/18.
+ */
+
+public class LocalMatch extends Activity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.local_match);
+    }
+}
